033b43ecbbb260a9424e89344e5addff130b989d
bnwlkr <bnwlkr@icloud.com>
2018-11-17 16:53:46 -0800

    added card values

--- poker.pl
+++ poker.pl
@@ -3,7 +3,6 @@ suit(♥).
 suit(♠).
 suit(♣).
 
-rank(  ace).
 rank(    2).
 rank(    3).
 rank(    4).
@@ -16,12 +15,17 @@ rank(   10).
 rank( jack).
 rank(queen).
 rank( king).
+rank(  ace).
 
 card(Rank, Suit) :- rank(Rank), suit(Suit).
 
-
 deck(A) :- findall((Suit, Rank), card(Suit, Rank), A).
 
+value(card(rank(jack), _), 11).
+value(card(rank(queen), _), 12).
+value(card(rank(king), _), 13).
+value(card(rank(ace), _), 14).
+value(card(rank(N), _), N) :- number(N).
 
 
 
