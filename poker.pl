6a05ae94c1955aedd9b72ab7074e04c6a1089627
bnwlkr <bnwlkr@icloud.com>
2018-11-17 23:33:31 -0800

    added some hands. just rough ideas for now.

--- poker.pl
+++ poker.pl
@@ -1,7 +1,5 @@
-suit(♦).
-suit(♥).
-suit(♠).
-suit(♣).
+% to set the current games pocket: asserta(pocket(card_,_), card(_,_)).
+% to set the table: retract + assert(table([card(_,_),...]))
 
 rank(    2).
 rank(    3).
@@ -17,20 +15,38 @@ rank(queen).
 rank( king).
 rank(  ace).
 
+suit(d).
+suit(h).
+suit(s).
+suit(c).
+
+order(suit(d), 0).
+order(suit(h), 1).
+order(suit(s), 2).
+order(suit(c), 3).
+
 card(Rank, Suit) :- rank(Rank), suit(Suit).
+
+value(card(jack, _), 11).
+value(card(queen, _), 12).
+value(card(king, _), 13).
+value(card(ace, _), 14).
+value(card(N, _), N) :- number(N), rank(N).
 
 deck(A) :- findall((Suit, Rank), card(Suit, Rank), A).
 
-value(card(rank(jack), _), 11).
-value(card(rank(queen), _), 12).
-value(card(rank(king), _), 13).
-value(card(rank(ace), _), 14).
-value(card(rank(N), _), N) :- number(N).
+% hands
+
+three(card(A,SX), card(A, SY), card(A, SZ)) :- dif(SX,SY), dif(SY, SZ), dif(SX,SZ).
+twopair(A,B,C,D) :- pair(A,B), pair(C,D).
+pair(card(A,SX), card(A,SY)) :- dif(SX,SY).
+high(card(_,_)).
 
 
 
 
-
+% finds pairs only for now (TODO: remove duplicates)
+hands(CARDS, R) :- findall(pair(A,B), (member(A, CARDS), member(B, CARDS), pair(A,B)), R).
 
 
 
