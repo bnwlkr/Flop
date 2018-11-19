04d3b5b170193681ac4fb3c0eab4d75b8b82c6f4
bnwlkr <bnwlkr@icloud.com>
2018-11-19 12:33:33 -0800

    added some more hands, and removed duplicates in pair

--- poker.pl
+++ poker.pl
@@ -20,11 +20,6 @@ suit(h).
 suit(s).
 suit(c).
 
-order(suit(d), 0).
-order(suit(h), 1).
-order(suit(s), 2).
-order(suit(c), 3).
-
 card(Rank, Suit) :- rank(Rank), suit(Suit).
 
 value(card(jack, _), 11).
@@ -35,18 +30,20 @@ value(card(N, _), N) :- number(N), rank(N).
 
 deck(A) :- findall((Suit, Rank), card(Suit, Rank), A).
 
+diff_order(SX, SY) :- dif(SX, SY), SX @< SY.
+
 % hands
-
-three(card(A,SX), card(A, SY), card(A, SZ)) :- dif(SX,SY), dif(SY, SZ), dif(SX,SZ).
-twopair(A,B,C,D) :- pair(A,B), pair(C,D).
-pair(card(A,SX), card(A,SY)) :- dif(SX,SY).
-high(card(_,_)).
+three(card(A,SX), card(A, SY), card(A, SZ)) :- diff_order(SX,SY), diff_order(SY, SZ).
+twopair(A,B,C,D) :- pair(A,B), pair(C,D), diff_order(A,C), dif(A,B), dif(A,C), dif(A,D), dif(B,C), dif(B,D).
+pair(card(A,SX), card(A,SY)) :- diff_order(SX, SY).
 
 
-
-
-% finds pairs only for now (TODO: remove duplicates)
+% return a list of pairs
 hands(CARDS, R) :- findall(pair(A,B), (member(A, CARDS), member(B, CARDS), pair(A,B)), R).
+% return a list of twopairs
+hands(CARDS, R) :- findall(twopair(A,B,C,D), (member(A, CARDS), member(B, CARDS), member(C, CARDS), member(D, CARDS), twopair(A,B,C,D)), R).
+% return a list of threes
+hands(CARDS, R) :- findall(three(A,B,C), (member(A, CARDS), member(B, CARDS), member(C, CARDS), three(A,B,C)), R).
 
 
 
