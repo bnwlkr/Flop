fc31440073aa3aeeae5ad3eb403279a13f5ae9a9
bnwlkr <bnwlkr@icloud.com>
2018-11-22 09:57:41 -0800

    added winner determination function, and tiebreakers for pairs and twopairs

--- poker.pl
+++ poker.pl
@@ -66,17 +66,11 @@ ranking(flush(_,_,_,_,_), 5).
 
 
 highs(CARDS, R) :- findall(high(A), (member(A, CARDS), high(A)), R).
-
 pairs(CARDS, R) :- findall(pair(A,B, CARDS), (member(A, CARDS), member(B, CARDS), pair(A,B)), R).
-
 twopairs(CARDS, R) :- findall(twopair(A,B,C,D), (member(A, CARDS), member(B, CARDS), member(C, CARDS), member(D, CARDS), twopair(A,B,C,D)), R).
-
 threes(CARDS, R) :- findall(three(A,B,C), (member(A, CARDS), member(B, CARDS), member(C, CARDS), three(A,B,C)), R).
-
 straights(CARDS, R) :- findall(straight(A,B,C,D,E), (member(A, CARDS), member(B, CARDS), member(C, CARDS), member(D, CARDS), member(E, CARDS), straight(A,B,C,D,E)), R).
-
 flushes(CARDS, R) :- findall(flush(A,B,C,D,E), (member(A, CARDS), member(B, CARDS), member(C, CARDS), member(D, CARDS), member(E, CARDS), flush(A,B,C,D,E)), R).
-
 hands(CARDS, R) :- highs(CARDS, HIGHS), pairs(CARDS, PAIRS), twopairs(CARDS, TWOPAIRS), threes(CARDS, THREES), straights(CARDS, STRAIGHTS), flushes(CARDS, FLUSHES), append(HIGHS, PAIRS, ONE), append(ONE, TWOPAIRS, TWO), append(TWO, THREES, THREE), append(THREE, STRAIGHTS, FOUR), append(FOUR, FLUSHES, R).
 
 % find best hand for individual player.
@@ -85,12 +79,32 @@ best([H|T],TM) :- ranking(H,HR), ranking(TM, TMR), TMR > HR, best(T, TM).
 best([H|T],H) :- ranking(H,HR), ranking(TM, TMR), TMR < HR, best(T, TM).
 best([H|T],TM) :- ranking(H,HR), ranking(TM, TMR), HR == TMR, best(T, TM), beats(TM, H).
 best([H|T],TM) :- ranking(H,HR), ranking(TM, TMR), HR == TMR, best(T, TM), beats(H, TM).
+% choose random card if neither beats the other
+best([H|T],TM) :- ranking(H,HR), ranking(TM, TMR), HR == TMR, best(T, TM), \+beats(H, TM), \+beats(TM, H).
 
-beats(high(A), high(B)) :- value(A,VA), value(B,VB), VA >= VB.
-beats(pair(A,_), pair(B,_)) :- value(A,VA), value(B,VB), VA >= VB.
+% determine superiority between similar hands
+beats(high(A), high(B)) :- value(A,VA), value(B,VB), VA > VB.
+beats(pair(A,_), pair(B,_)) :- value(A,VA), value(B,VB), VA > VB.
+beats(twopair(A1,_,_,_), twopair(A2,_,B2,_)) :- value(A1, VA1), value(A2, VA2), value(B2,VB2), VA1 > VA2, VA1 > VB2.
+beats(twopair(_,_,B1,_), twopair(A2,_,B2,_)) :- value(A2, VA2), value(B1, VB1), value(B2,VB2), VB1 > VA2, VB1 > VB2.
 
-tiebreak(pair(A1,B1), pair(A2,B2), CARDSP1, CARDP2) :- subtract(CARDSP1, [A1,B1], P1), subtract(CARDS, [A2,B2], P2), cardsum(P1, X1), cardsum(P2, X2), X1 >= X2.
+% get max rank from a set of cards
+maxrank([C], R) :- value(C, R).
+maxrank([H|T], RR) :- maxcard(T, RR), value(H, HR), RR >= HR.
+maxrank([H|T], HR) :- maxcard(T, RR), value(H, HR), HR >= RR.
 
-% sum of card values for tiebreaks
-cardsum([C], X) :- value(C,X).
-cardsum([H|T], X) :- value(H,V), cardsum(T, Q), X is V + Q.
+% tiebreak between same hands
+tiebreak(pair(A1,B1), pair(A2,B2), CARDSP1, CARDSP2) :- subtract(CARDSP1, [A1,B1], RP1), subtract(CARDSP2, [A2,B2], RP2), maxcard(RP1, MRP1), maxcard(RP2, MRP2), MRP1 > MRP2.
+tiebreak(twopair(A1,B1,C1,D1), twopair(A2,B2,C2,D2), CARDSP1, CARDSP2) :- subtract(CARDSP1, [A1,B1,C1,D1], RP1), subtract(CARDSP2, [A2,B2,C2,D2], RP2), maxcard(RP1, MRP1), maxcard(RP2, MRP2), MRP1 > MRP2.
+
+% W is 1 if P1 wins, and 2 if P2 wins
+winner(CARDSP1, CARDSP2, 1) :- hands(CARDSP1, HANDSP1), hands(CARDSP2, HANDSP2), append(HANDSP1, HANDSP2, ALLHANDS), best(ALLHANDS, X), member(X, HANDSP1).
+winner(CARDSP1, CARDSP2, 2) :- hands(CARDSP1, HANDSP1), hands(CARDSP2, HANDSP2), append(HANDSP1, HANDSP2, ALLHANDS), best(ALLHANDS, X), member(X, HANDSP2).
+% neither have a winning hand, go to tiebreak
+winner(CARDSP1, CARDSP2, 1) :- hands(CARDSP1, HANDSP1), hands(CARDSP2, HANDSP2), best(HANDSP1, BESTP1), best(HANDSP2, BESTP2), tiebreak(BESTP1, BESTP2, CARDSP1, CARDSP2).
+winner(CARDSP1, CARDSP2, 2) :- hands(CARDSP1, HANDSP1), hands(CARDSP2, HANDSP2), best(HANDSP1, BESTP1), best(HANDSP2, BESTP2), tiebreak(BESTP2, BESTP1, CARDSP2, CARDSP1).
+% there could be no winner (split pot).
+
+
+
+
