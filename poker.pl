380eb9c180108d403a52f82f901feed041eeabe0
bnwlkr <bnwlkr@icloud.com>
2018-11-22 00:22:43 -0800

    finding best hand is on its way, started with some tiebreak logic

--- poker.pl
+++ poker.pl
@@ -1,6 +1,14 @@
+:- use_module(library(lists)).
+
 % to set the current games pocket: asserta(pocket(card_,_), card(_,_)).
 % to set the table: retract + assert(table([card(_,_),...]))
 % @< used as arbitrary ordering to remove duplicate answers
+
+
+% test card set
+test([card(3,d), card(4,h)]).
+
+
 
 rank(    2).
 rank(    3).
@@ -49,7 +57,6 @@ pair(card(A,SX), card(A,SY)) :- SX @< SY.
 
 high(card(_,_)).
 
-
 ranking(high(_), 0).
 ranking(pair(_,_), 1).
 ranking(twopair(_,_,_,_), 2).
@@ -60,7 +67,7 @@ ranking(flush(_,_,_,_,_), 5).
 
 highs(CARDS, R) :- findall(high(A), (member(A, CARDS), high(A)), R).
 
-pairs(CARDS, R) :- findall(pair(A,B), (member(A, CARDS), member(B, CARDS), pair(A,B)), R).
+pairs(CARDS, R) :- findall(pair(A,B, CARDS), (member(A, CARDS), member(B, CARDS), pair(A,B)), R).
 
 twopairs(CARDS, R) :- findall(twopair(A,B,C,D), (member(A, CARDS), member(B, CARDS), member(C, CARDS), member(D, CARDS), twopair(A,B,C,D)), R).
 
@@ -70,21 +77,20 @@ straights(CARDS, R) :- findall(straight(A,B,C,D,E), (member(A, CARDS), member(B,
 
 flushes(CARDS, R) :- findall(flush(A,B,C,D,E), (member(A, CARDS), member(B, CARDS), member(C, CARDS), member(D, CARDS), member(E, CARDS), flush(A,B,C,D,E)), R).
 
-
 hands(CARDS, R) :- highs(CARDS, HIGHS), pairs(CARDS, PAIRS), twopairs(CARDS, TWOPAIRS), threes(CARDS, THREES), straights(CARDS, STRAIGHTS), flushes(CARDS, FLUSHES), append(HIGHS, PAIRS, ONE), append(ONE, TWOPAIRS, TWO), append(TWO, THREES, THREE), append(THREE, STRAIGHTS, FOUR), append(FOUR, FLUSHES, R).
 
-% THIS STUFF DOESNT WORK BECAUSE OF THE WAY THAT TIES ARE BROKEN...
+% find best hand for individual player.
+best([C],C).
+best([H|T],TM) :- ranking(H,HR), ranking(TM, TMR), TMR > HR, best(T, TM).
+best([H|T],H) :- ranking(H,HR), ranking(TM, TMR), TMR < HR, best(T, TM).
+best([H|T],TM) :- ranking(H,HR), ranking(TM, TMR), HR == TMR, best(T, TM), beats(TM, H).
+best([H|T],TM) :- ranking(H,HR), ranking(TM, TMR), HR == TMR, best(T, TM), beats(H, TM).
 
-%best([C], C).
-%best([H|T], SF) :- ranking(H, RANK), ranking(SF, SFRANK), RANK == SFRANK, beats(H,SF), best(T, H).
-%best([H|T], SF) :- ranking(H, RANK), ranking(SF, SFRANK), RANK == SFRANK, beats(SF,H), best(T, SF).
-%best([H|T], SF) :- ranking(H, RANK), ranking(SF, SFRANK), RANK > SFRANK, best(T, H).
-%best([H|T], SF) :- ranking(H, RANK), ranking(SF, SFRANK), RANK < SFRANK, best(T, SF).
+beats(high(A), high(B)) :- value(A,VA), value(B,VB), VA >= VB.
+beats(pair(A,_), pair(B,_)) :- value(A,VA), value(B,VB), VA >= VB.
 
-%beats(high(A), high(B)) :- value(A, VA), value(B, VB), VA > VB.
-%beats(pair(A,_), pair(B,_)) :- value(A, VA), value(B, VB), VA > VB.
-%beats(twopair(A,_,C,_), twopair(B,_,D,_)) :- value(A,VA), value(B,VB), value(C,VC), value(D,VD), VA + VC < VB + VD.
-%beats(three(A,_,_), three(B,_,_)) :- value(A, VA), value(B, VB), VA > VB.
-%beats(straight(A,_,_,_,_), straight(B,_,_,_,_)) :- value(A, VA), value(B, VB), VA > VB.
-%beats(straight(A,B,C,D,_), straight(B,_,_,_,_))
+tiebreak(pair(A1,B1), pair(A2,B2), CARDSP1, CARDP2) :- subtract(CARDSP1, [A1,B1], P1), subtract(CARDS, [A2,B2], P2), cardsum(P1, X1), cardsum(P2, X2), X1 >= X2.
 
+% sum of card values for tiebreaks
+cardsum([C], X) :- value(C,X).
+cardsum([H|T], X) :- value(H,V), cardsum(T, Q), X is V + Q.
