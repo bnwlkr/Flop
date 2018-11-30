34c669b3130ee95cfff9117fd07b5d0fda2516b3
bnwlkr <bnwlkr@icloud.com>
2018-11-29 19:04:50 -0800

    added topN function to find highest ranked set of cards

--- probability.pl
+++ probability.pl
@@ -13,3 +13,7 @@ nottaken(TAKEN, NOTTAKEN) :- allcards(ALL), subtract(ALL, TAKEN, NOTTAKEN).
 possiblehands(NOTTAKEN, R) :- findall([card(V1, S1), card(V2, S2)], (member(card(V1, S1), NOTTAKEN), member(card(V2, S2), NOTTAKEN), greater(card(V1, S1), card(V2, S2))), R).
 
 chancetowin(MYHAND, COMM, NUMPLAYERS) :- append(MYHAND, COMM, MYCARDS), nottaken(MYCARDS, NOTTAKEN), possiblehands(NOTTAKEN, HANDS), whatbeatsme(MYHAND, COMM, HANDS, BEATSME), length(HANDS, T), length(BEATSME, B), write("There are "), write(T), write(" total possible hands and "), write(B), write(" that beat yours.\n").
+
+topN(N, _, Xs) :- N =< 0, !, N =:= 0, Xs = [].
+topN(_, [], []).
+topN(N, CARDS, [X|Ys]) :- M is N-1, maxrank(CARDS, X), select(X, CARDS, REST), topN(M, REST, Ys).
